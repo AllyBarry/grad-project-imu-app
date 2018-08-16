483cf172ff8a79c4ed00cf69f49ed55dd241b89c
AllyBarry <ali.barry3@gmail.com>
2018-08-16 10:55:32 +0200

    AddedProject

---
+++ lib/customw.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class customwidgets extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.pink,
+      child: new Container(
+        color: Colors.orange,
+        margin: const EdgeInsets.all(15.0),
+        child: new Container(
+          color: Colors.blue,
+          margin: const EdgeInsets.all(15.0),
+          child: _methodcustom(),
+        ),
+      ),
+
+    );
+  }
+}
+
+Widget _methodcustom(){
+  return new Container(
+    color: Colors.purple,
+    margin: const EdgeInsets.all(15.0),
+  );
+}
